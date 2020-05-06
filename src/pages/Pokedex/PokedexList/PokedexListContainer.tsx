3622b8c0de44a67dc6da7fdac47171c60e3916d1
Ivo Verburgh <ivo@skednet.com>
2020-05-06 19:51:04 +0200

    working pokedex

--- src/pages/Pokedex/PokedexList/PokedexListContainer.tsx
+++ src/pages/Pokedex/PokedexList/PokedexListContainer.tsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PokedexList from "./PokedexList";
 import { Pokemon } from "../../../models/pokemon";
+import axios from "axios";
+import { Trainer } from "../../../models/trainer";
 
 const PokedexListContainer = () => {
-  let pokemonList: Pokemon[] = [];
-  const pokedex: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [trainer, setTrainer] = useState<Trainer>();
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>();
 
-  return (
-    <PokedexList loading={false} pokemonList={pokemonList} pokedex={pokedex} />
-  );
+  useEffect(() => {
+    getPokemonList();
+    getTrainer();
+  }, [trainer, pokemonList]);
+
+  const getTrainer = () => {
+    axios
+      .get("http://localhost:3000/trainer/5eb069b0eabc101abc927af5")
+      .then((response) => {
+        setTrainer(response.data);
+      });
+  };
+
+  const getPokemonList = () => {
+    axios.get("http://localhost:3000/pokemon/").then((response) => {
+      setPokemonList(response.data);
+    });
+  };
+
+  if (pokemonList && trainer) {
+    return (
+      <PokedexList
+        loading={false}
+        pokemonList={pokemonList}
+        pokedex={trainer.trainerpokemons.map((tp) => tp.pokemonnumber)}
+      />
+    );
+  } else {
+    return <PokedexList loading={true} pokemonList={[]} pokedex={[]} />;
+  }
 };
 
 export default PokedexListContainer;
